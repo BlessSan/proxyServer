3061a7d26e635eb72e81e89295f9e738dd3f52b7
BlessSan <87181251+BlessSan@users.noreply.github.com>
2022-02-23 23:28:36 +0800

    installed and added cors middleware

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
@@ -9,6 +10,7 @@ const HOST = process.env.HOST || "0.0.0.0";
 const API_SERVICE_URL = "https://n7b67.sse.codesandbox.io";
 
 app.use(morgan("dev"));
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello");
